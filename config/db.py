315204b6a5af60384ad448d67add44d43f9d6bae
Ana Karen Crisanto Reyes <anitacrisanto0@gmail.com>
2025-03-19 23:44:36 -0600

    Subiendo actualizacion nueva de env

--- config/db.py
+++ config/db.py
@@ -1,18 +1,15 @@
-"""
-Módulo db.py
-
-Este módulo configura la conexión a la base de datos utilizando SQLAlchemy.
-Define el motor de la base de datos, la sesión local y la clase base para los modelos.
-"""
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
 
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
-# URL de conexión a la base de datos MySQL
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/base_bd"
+# Obtener la URL de conexión desde las variables de entorno
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Crear el motor de la base de datos
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -22,7 +19,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Clase base para los modelos SQLAlchemy
 Base = declarative_base()
-
 
 # Función para obtener la sesión de la base de datos
 def get_db():
