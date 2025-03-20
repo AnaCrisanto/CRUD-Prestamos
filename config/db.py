1a228bf8196ee2ce1159a765045bdb3c6c695f47
Ana Karen Crisanto Reyes <pananaxdana@gmail.com>
2025-03-20 11:36:08 -0600

    Corrigiendo el error de localhost

--- config/db.py
+++ config/db.py
@@ -1,15 +1,24 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from passlib.context import CryptContext
 from dotenv import load_dotenv
 import os
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
-# Obtener la URL de conexión desde las variables de entorno
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+# Obtener las credenciales desde las variables de entorno
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Construir la URL de conexión SIN SSL
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 # Crear el motor de la base de datos
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Permite tu frontend en Vue
+    allow_origins=["https://main--prestamosana.netlify.app"],  # Permite tu frontend en Vue
     allow_credentials=True,
     allow_methods=["*"],  # Permite todos los métodos HTTP
     allow_headers=["*"],  # Permite todos los headers
